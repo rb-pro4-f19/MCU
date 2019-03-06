4c5e6d029c6eed2f6cc50a2793519bd4d7c815f1
Martin Androvich <martinandrovich@gmail.com>
2019-03-06 22:38:31 +0100

    Add SPI transmission test

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
 #include "spi.h"
 
 int main(void)
 {
 	SPI* test = spi.new(2, 500);
+
+	for(;;)
+	{
+		spi.send(test, MOT1, 0b00110010, false);
+		for (int i = 0; i < 1000; i++);
+	}
+
 	return 0;
 }
