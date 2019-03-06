929fefef163387a79d2344780da3736a9cdd0c91
Martin Androvich <martinandrovich@gmail.com>
2019-03-06 15:33:36 +0100

    Update main.c

--- sandbox/Checksum/main.c
+++ sandbox/Checksum/main.c
@@ -4,24 +4,23 @@
 
 // prototypes
 void	printf_binary(uint16_t v);
-
 uint8_t	bsd_chksum_4bit(uint16_t frame, int num_of_nibbles);
 uint8_t extract_nibble(uint16_t data, int pos);
 uint8_t ror_nibble(uint8_t data);
 uint8_t rol_nibble(uint8_t data);
 
-uint8_t bsd_chksum_4bit(uint16_t frame, int num_of_nibbles)
+uint8_t bsd_chksum_4bit(uint16_t data, int num_of_nibbles)
 {
 	// https://en.wikipedia.org/wiki/BSD_checksum
 
 	// variables
-	uint8_t checksum = 0b0000;
+	uint8_t checksum = 0;
 
-	// algorithm starting from MSB
+	// algorithm starting from Most Signifigant Nibble
 	for (int i = num_of_nibbles; i >= 0; --i)
 	{
 		checksum = ror_nibble(checksum);
-		checksum = checksum + extract_nibble(frame, i);
+		checksum = checksum + extract_nibble(data, i);
 		checksum = checksum & 0xF;
 	}
 
@@ -47,34 +46,39 @@ uint8_t extract_nibble(uint16_t data, int pos)
 	// extract nibble at position n
 	// test = 0101 1111 1010 0000 -> extract_nibble(test, 2) = 1111
 
-	return ((data & (0xF << pos * 4)) >> pos * 4);
+	return ((data & (0xF << pos * 4)) >> (pos * 4));
 }
 
 uint8_t rol_nibble(uint8_t data)
 {
-	return (data << 1) | (data >> 3) & 0xF;
+	return ((data << 1) | (data >> 3) & 0xF);
 }
 
 uint8_t ror_nibble(uint8_t data)
 {
-	return (data >> 1) | (data << 3) & 0xF;
+	return ((data >> 1) | (data << 3) & 0xF);
 }
 
 int main(void)
 {	
-	uint16_t tstframe	= 0b101110001110;
-	uint16_t tstchksum	= bsd_chksum_4bit(tstframe, 3);
+	// 12 bit checksum test
+	uint16_t test12bit_val = 0b101110001110;
+	uint16_t test12bit_sum = bsd_chksum_4bit(test12bit_val, 3);
 
-	uint16_t tstframe2	= 0b1011100011100000;
-	uint16_t tstchksum2 = bsd_chksum_4bit(tstframe2, 4);
-
-	printf_binary(tstframe);
-	printf_binary(tstchksum);
+	printf("12-bit checksum\n");
+	printf_binary(test12bit_val);
+	printf_binary(test12bit_sum);
 
 	printf("\n");
 
-	printf_binary(tstframe2);
-	printf_binary(tstchksum2);
+	// 16 bit checksum test
+	uint16_t test16bit_val = 0b1011100011100000;
+	uint16_t test16bit_sum = bsd_chksum_4bit(test16bit_val, 4);
 
+	printf("16-bit checksum\n");
+	printf_binary(test16bit_val);
+	printf_binary(test16bit_sum);
+
+	// stall
 	getchar();
 }
