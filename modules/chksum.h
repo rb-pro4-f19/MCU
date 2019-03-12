438f0d0c1335fc8794ce414f893340fec6a92a1e
Martin Androvich <martinandrovich@gmail.com>
2019-03-12 23:18:21 +0100

    Removed illegal enum

--- modules/chksum.h
+++ modules/chksum.h
@@ -24,19 +24,13 @@
 
 /*****************************    Defines    *******************************/
 
-enum CHKSUM_SIZE CHKSUM_SIZE
+typedef enum CHKSUM_SIZE CHKSUM_SIZE;
 
 /***********************     External Variables     ************************/
 
 /*****************************   Constants   *******************************/
 
 /*************************    Class Functions    ***************************/
-
-enum CHKSUM_SIZE
-{
-	4BIT = 4,
-	8BIT = 8
-};
 
 extern const struct CHECKSUM_CLASS
 {
