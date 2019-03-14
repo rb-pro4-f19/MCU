af3a58dd371b421abcd8a4e9b3c00229db86334f
Martin Androvich <martinandrovich@gmail.com>
2019-03-15 00:57:15 +0100

    Updated CHECKSUM to v.1.2.1
    Added 8-bit support and variable data length.

--- ccs/test/chksum_test.c
+++ ccs/test/chksum_test.c
@@ -6,19 +6,25 @@
 
 void chksum_test(void)
 {
+    // chksum.h
+    // version 1.2.1
+
+    // WARNING!
+    // rewrite "0b0011'0101" to "0b00110101" when using C
+
     // 4-bit checksum test
-    // input:   1011 1000 1110
+    // input:   1011'1000'1110
     // size:    3 nibbles (12 bits)
     // output:  1000
 
     uint16_t data_4bit      = 0b101110001110;
     uint8_t  chks_4bit      = chksum2.gen_4bit(data_4bit, 3);
-    bool     vali_4bit      = chksum2.val_8bit(data_4bit, 2, chks_4bit);
+    bool     vali_4bit      = chksum2.val_4bit(data_4bit, 2, chks_4bit);
 
     // 8-bit checksum test
-    // input:   10101011 10001110
+    // input:   1010'1011 ; 1000'1110
     // size:    2 bytes (16 bits)
-    // output:  11010110
+    // output:  0010'0101
 
     uint8_t  data_8bit[2]   = { 0b10101011, 0b10001110 };
     uint8_t  chks_8bit      = chksum2.gen_8bit(data_8bit, 2);
--- sandbox/Checksum/main.c
+++ sandbox/Checksum/main.c
@@ -8,13 +8,13 @@
 int main(void)
 {	
 	// 4-bit checksum test
-	// input:   1011 1000 1110
+	// input:   1011'1000'1110
 	// size:    3 nibbles (12 bits)
 	// output:  1000
 
-	uint16_t data_4bit		= 0b101110001110;
+	uint16_t data_4bit		= 0b1011'1000'1110;
 	uint8_t  chks_4bit		= chksum2.gen_4bit(data_4bit, 3);
-	bool	 vali_4bit		= chksum2.val_8bit(data_4bit, 2, chks_4bit);
+	bool	 vali_4bit		= chksum2.val_4bit(data_4bit, 2, chks_4bit);
 	
 	printf("[4-bit checksum]\n\n");
 
@@ -30,11 +30,11 @@ int main(void)
 	printf("\n");
 
 	// 8-bit checksum test
-	// input:   10101011 10001110
+	// input:   1010'1011 ; 1000'1110
 	// size:    2 bytes (16 bits)
-	// output:  00100101
+	// output:  0010'0101
 
-	uint8_t  data_8bit[2]	= { 0b10101011, 0b10001110 };
+	uint8_t  data_8bit[2]	= { 0b1010'1011, 0b1000'1110 };
 	uint8_t	 chks_8bit		= chksum2.gen_8bit(data_8bit, 2);
 	bool	 vali_8bit		= chksum2.val_8bit(data_8bit, 2, chks_8bit);
 
