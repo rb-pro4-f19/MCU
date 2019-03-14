51e7867a69bc42ca90e0a2c78c0dbcd5817ee533
Martin Androvich <martinandrovich@gmail.com>
2019-03-14 21:17:49 +0100

    Checksum v.1.2.0 testing

--- ccs/test/chksum_test.c
+++ ccs/test/chksum_test.c
@@ -2,17 +2,25 @@
 #include <stdbool.h>
 
 #include "spi.h"
-#include "chksum.h"
+#include "chksum2.h"
 
 void chksum_test(void)
 {
+    // 4-bit checksum test
+    // input:   1011 1000 1110
+    // size:    3 nibbles (12 bits)
+    // output:  1000
 
-    // construct test frame
-    SPI_FRAME	frm1  = {ENC1, 170, 0};
-    bool		frm1_is_valid;
+    uint16_t data_4bit      = 0b101110001110;
+    uint8_t  chks_4bit      = chksum2.gen_4bit(data_4bit, 3);
+    bool     vali_4bit      = chksum2.val_8bit(data_4bit, 2, chks_4bit);
 
-    // generate & validate checksums
-    frm1.chksum		= chksum.generate(&frm1);
-    frm1_is_valid	= chksum.validate(&frm1);
+    // 8-bit checksum test
+    // input:   10101011 10001110
+    // size:    2 bytes (16 bits)
+    // output:  11010110
 
+    uint8_t  data_8bit[2]   = { 0b10101011, 0b10001110 };
+    uint8_t  chks_8bit      = chksum2.gen_8bit(data_8bit, 2);
+    bool     vali_8bit      = chksum2.val_8bit(data_8bit, 2, chks_8bit);
 }
